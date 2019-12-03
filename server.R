3c568c5ecbcbb97f28fc99ee253ea0a964b87c99
Funke Alliyu <fun.k@Funkes-MacBook-Air.local>
2019-12-03 13:23:10 -0500

    code editing

--- server.R
+++ server.R
@@ -708,7 +708,8 @@ shinyServer(
             description = "NA"
           ),
           result = list(
-            success = if(score > 0) TRUE else FALSE,
+            success = if(score > 
+                         0) TRUE else FALSE,
             response = "SOMETHING"
           )
         )
