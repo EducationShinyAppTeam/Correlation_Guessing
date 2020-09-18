032bea7810b9db754d35a26826ca0ba50f9f26ec
ROBERT PATRICK CAREY III <rpc5102@psu.edu>
2020-09-18 12:46:53 -0400

    Pass through Styler

--- app.R
+++ app.R
@@ -8,28 +8,28 @@ library(ggplot2)
 
 ## App Meta Data----------------------------------------------------------------
 APP_TITLE <<- "Correlation Guessing"
-APP_DESCP  <<- paste(
+APP_DESCP <<- paste(
   "This app helps a student better understand the numerical value of correlations",
   "for scatterplots with or without outliers."
 )
 ## End App Meta Data------------------------------------------------------------
 
 # Define global constants and functions ----
-generateData <- function(difficulty){
-  if(as.numeric(difficulty) == 3) {
+generateData <- function(difficulty) {
+  if (as.numeric(difficulty) == 3) {
     difficulty <- sample(1:2, 1)
-    numPoints <- ifelse(as.numeric(difficulty) == 1, 50, sample(5:25, 1) )
+    numPoints <- ifelse(as.numeric(difficulty) == 1, 50, sample(5:25, 1))
   } else {
-    numPoints <- ifelse(as.numeric(difficulty) == 1, 50, sample(5:25, 1) )
+    numPoints <- ifelse(as.numeric(difficulty) == 1, 50, sample(5:25, 1))
   }
   value <- rnorm(1, 0, 10)
   valueB <- rnorm(1, 4, 1)
-  outlier <- as.numeric(sample(2:6, 1)) #create a random number
-  if (difficulty == 2){ # with outlier
+  outlier <- as.numeric(sample(2:6, 1)) # create a random number
+  if (difficulty == 2) { # with outlier
     choice <- sample(2, 1)
-    if (choice == 2){
+    if (choice == 2) {
       X1 <- rnorm(numPoints, value, valueB)
-      Y1 <- rnorm(numPoints, rnorm(1) * X1, rgamma(1, 1)*valueB)
+      Y1 <- rnorm(numPoints, rnorm(1) * X1, rgamma(1, 1) * valueB)
       mux <- mean(X1)
       sdx <- sd(X1)
       outx <- mux + (outlier * sdx) # function for outlier
@@ -37,10 +37,10 @@ generateData <- function(difficulty){
       muy <- mean(Y1)
       sdy <- sd(Y1)
       outy <- muy + (outlier * sdy)
-      Y = c(Y1, outy)
+      Y <- c(Y1, outy)
       return(data.frame(X, Y))
     }
-    else{
+    else {
       X1 <- rnorm(numPoints, value, valueB)
       Y1 <- rnorm(numPoints, rnorm(1) * X1, rgamma(1, 1) * valueB)
       mux <- mean(X1)
@@ -54,7 +54,7 @@ generateData <- function(difficulty){
       return(data.frame(X, Y))
     }
   }
-  else if (difficulty == 1){
+  else if (difficulty == 1) {
     X <- rnorm(numPoints, value, valueB)
     Y <- rnorm(numPoints, rnorm(1) * X, rgamma(1, 1) * valueB)
     return(data.frame(X, Y))
@@ -69,13 +69,13 @@ gradeEstimate <- function(user, corr) {
     icon = "default",
     message = "feedback"
   )
-  if (0.9 <= abs(corr) && abs(corr) <= 1){
+  if (0.9 <= abs(corr) && abs(corr) <= 1) {
     if (user * corr < 0) {
       output$heartChange <- -1
       output$scoreChange <- -5
       output$icon <- "incorrect"
       output$message <- "Check your sign."
-    } else if (diff < 0.05){
+    } else if (diff < 0.05) {
       output$heartChange <- 1
       output$scoreChange <- 5
       output$icon <- "correct"
@@ -97,12 +97,12 @@ gradeEstimate <- function(user, corr) {
       output$scoreChange <- -5
       output$icon <- "incorrect"
       output$message <- "Check your sign."
-    } else if(abs(user) == 1 && diff < 0.2){
+    } else if (abs(user) == 1 && diff < 0.2) {
       output$heartChange <- 0
       output$scoreChange <- 0
       output$icon <- "partial"
       output$message <- "You're just a bit off."
-    } else if (diff < 0.1){
+    } else if (diff < 0.1) {
       output$heartChange <- 0
       output$scoreChange <- 5
       output$icon <- "correct"
@@ -124,7 +124,7 @@ gradeEstimate <- function(user, corr) {
       output$scoreChange <- -5
       output$icon <- "incorrect"
       output$message <- "Check your sign."
-    } else if (diff < 0.2){
+    } else if (diff < 0.2) {
       output$heartChange <- 0
       output$scoreChange <- 5
       output$icon <- "correct"
@@ -146,7 +146,7 @@ gradeEstimate <- function(user, corr) {
       output$scoreChange <- -5
       output$icon <- "incorrect"
       output$message <- "Check your sign."
-    } else if (diff < 0.2){
+    } else if (diff < 0.2) {
       output$heartChange <- 0
       output$scoreChange <- 5
       output$icon <- "correct"
@@ -169,7 +169,8 @@ gradeEstimate <- function(user, corr) {
 ## Base performance plot ----
 basePerformPlot <- ggplot(
   data = data.frame(x = seq(from = -1, to = 1), y = seq(from = -1, to = 1)),
-  mapping = aes(x = x, y = y)) +
+  mapping = aes(x = x, y = y)
+) +
   geom_abline(slope = 1, intercept = 0) +
   theme_bw() +
   xlab("True Correlation") +
@@ -185,32 +186,43 @@ basePerformPlot <- ggplot(
 ui <- list(
   dashboardPage(
     skin = "yellow",
-    #Dashboard Header ----
+    # Dashboard Header ----
     dashboardHeader(
       titleWidth = 250,
       title = "Correlation Guessing",
-      tags$li(class = "dropdown",
-              actionLink("info",icon("info",class = "myClass"))),
       tags$li(
         class = "dropdown",
-        tags$a(href = "https://shinyapps.science.psu.edu/",
-               icon("home", lib = "font-awesome"))
+        actionLink("info", icon("info", class = "myClass"))
+      ),
+      tags$li(
+        class = "dropdown",
+        tags$a(
+          href = "https://shinyapps.science.psu.edu/",
+          icon("home", lib = "font-awesome")
+        )
       )
     ),
-    #Dashboard Sidebar ----
+    # Dashboard Sidebar ----
     dashboardSidebar(
       width = 250,
-      sidebarMenu(id = "tabs",
-                  menuItem("Overview", tabName = "overview",
-                           icon = icon("tachometer-alt")),
-                  menuItem("Game", tabName = "game", icon = icon("gamepad")),
-                  menuItem("References", tabName = "References",
-                           icon = icon("leanpub"))
+      sidebarMenu(
+        id = "tabs",
+        menuItem("Overview",
+          tabName = "overview",
+          icon = icon("tachometer-alt")
+        ),
+        menuItem("Game", tabName = "game", icon = icon("gamepad")),
+        menuItem("References",
+          tabName = "References",
+          icon = icon("leanpub")
+        )
       ),
-      tags$div(class = "sidebar-logo",
-               boastUtils::psu_eberly_logo("reversed"))
+      tags$div(
+        class = "sidebar-logo",
+        boastUtils::psu_eberly_logo("reversed")
+      )
     ),
-    #Dashboard Body
+    # Dashboard Body
     dashboardBody(
       tabItems(
         ## First tab - Overview ----
@@ -241,7 +253,7 @@ ui <- list(
           p("Make use of the Performance plot to track how you are doing over
             many tries."),
           div(
-            style = "text-align: center;" ,
+            style = "text-align: center;",
             bsButton(
               inputId = "start",
               label = "GO!",
@@ -249,7 +261,7 @@ ui <- list(
               icon = icon("bolt")
             )
           ),
-          #Acknowledgement
+          # Acknowledgement
           br(),
           br(),
           h2("Acknowledgements"),
@@ -269,10 +281,10 @@ ui <- list(
         ## Second tab - Game ----
         tabItem(
           tabName = "game",
-          h2('Find the appropriate correlation'),
+          h2("Find the appropriate correlation"),
           fluidRow(
             column(
-              #choose mode
+              # choose mode
               width = 3,
               wellPanel(
                 div(
@@ -286,13 +298,13 @@ ui <- list(
                       "Surprise" = 3
                     )
                   ),
-                  #checkbox for regression line
+                  # checkbox for regression line
                   checkboxInput(
                     inputId = "options",
                     label = "Show regression line"
                   ),
                   br(),
-                  align = 'center',
+                  align = "center",
                   sliderInput(
                     inputId = "slider",
                     label = "Select the correlation",
@@ -324,12 +336,14 @@ ui <- list(
               width = 6,
               align = "center",
               # Scatter plot
-              plotOutput(outputId = "plot1", width = '100%'),
+              plotOutput(outputId = "plot1", width = "100%"),
               bsPopover(
                 id = "plot1",
-                title =  "Scatterplot",
-                content = paste("Move the slide bar on the left",
-                                "to guess the correlation"),
+                title = "Scatterplot",
+                content = paste(
+                  "Move the slide bar on the left",
+                  "to guess the correlation"
+                ),
                 place = "top",
                 trigger = "hover"
               ),
@@ -345,7 +359,7 @@ ui <- list(
                 )
               ),
               # Track performance plot
-              plotOutput(outputId = "plot2", width = '100%'),
+              plotOutput(outputId = "plot2", width = "100%"),
               bsPopover(
                 id = "plot2",
                 title = "Performance Plot",
@@ -355,7 +369,8 @@ ui <- list(
                   "Red dots represents the difficulty level of Without Outliers,",
                   "Blue triangles represent the difficulty of With Outliers,",
                   "and Purple Squares represent the Surprise Difficulty level",
-                  "where you might have Outliers."),
+                  "where you might have Outliers."
+                ),
                 placement = "top",
                 trigger = "hover"
               ),
@@ -376,10 +391,10 @@ ui <- list(
               width = 3,
               wellPanel(
                 textOutput("score"),
-                uiOutput('heart'),
+                uiOutput("heart"),
                 bsButton(
                   inputId = "reset",
-                  label =  "Reset",
+                  label = "Reset",
                   style = "danger",
                   size = "large"
                 )
@@ -391,37 +406,37 @@ ui <- list(
         tabItem(
           tabName = "References",
           h2("References"),
-          p(     #shinyBS
+          p( # shinyBS
             class = "hangingindent",
             "Bailey, E. (2015), shinyBS: Twitter bootstrap components for shiny,
              R package. Available from
             https://CRAN.R-project.org/package=shinyBS"
           ),
-          p(     #Boast Utilities
+          p( # Boast Utilities
             class = "hangingindent",
             "Carey, R. (2019), boastUtils: BOAST Utilities, R Package.
              Available from
             https://github.com/EducationShinyAppTeam/boastUtils"
           ),
-          p(     #reference for ideas
+          p( # reference for ideas
             class = "hangingindent",
             "Chance, B., and Chance, F. (2014), Guess the Correlation Applet.
             Available from
             http://www.rossmanchance.com/applets/GuessCorrelation.html"
           ),
-          p(     #shinydashboard
+          p( # shinydashboard
             class = "hangingindent",
             "Chang, W. and Borges Ribeio, B. (2018), shinydashboard: Create
             dashboards with 'Shiny', R Package. Available from
             https://CRAN.R-project.org/package=shinydashboard"
           ),
-          p(     #shiny
+          p( # shiny
             class = "hangingindent",
             "Chang, W., Cheng, J., Allaire, J., Xie, Y., and McPherson, J.
             (2019), shiny: Web application framework for R, R Package.
             Available from https://CRAN.R-project.org/package=shiny"
           ),
-          p(     #shinyWidgets
+          p( # shinyWidgets
             class = "hangingindent",
             "Perrier, V., Meyer, F., Granjon, D., Fellows, I., and Davis, W.
             (2020), shinyWidgets: Custom Inputs Widgets for Shiny, R package.
@@ -453,7 +468,7 @@ server <- function(input, output, clientData, session) {
   currentPlot <- reactiveVal()
 
   # Info message ----
-  observeEvent(input$info,{
+  observeEvent(input$info, {
     sendSweetAlert(
       session = session,
       title = "Instructions",
@@ -524,13 +539,13 @@ server <- function(input, output, clientData, session) {
     })
 
     ##### The max heart is five
-    if(hearts() > 5){
+    if (hearts() > 5) {
       hearts(5)
     }
   })
 
   ## Reset button ----
-  observeEvent(input$reset,{
+  observeEvent(input$reset, {
     score(0)
     hearts(5)
 
@@ -540,12 +555,14 @@ server <- function(input, output, clientData, session) {
 
     ### Create data ----
     data <- generateData(input$difficulty)
-    correlation(round(cor(data[, 1],data[, 2]), 2))
+    correlation(round(cor(data[, 1], data[, 2]), 2))
 
     ### Make Plots ----
     currentPlot(
-      ggplot(data = data,
-             mapping = aes(x = X, y = Y)) +
+      ggplot(
+        data = data,
+        mapping = aes(x = X, y = Y)
+      ) +
         geom_point(color = psuPalette[4], size = 4) +
         theme_bw() +
         labs(title = "Current Scatterplot") +
@@ -584,12 +601,14 @@ server <- function(input, output, clientData, session) {
 
     ### Create data ----
     data <- generateData(input$difficulty)
-    correlation(round(cor(data[, 1],data[, 2]), 2))
+    correlation(round(cor(data[, 1], data[, 2]), 2))
 
     ### Make Plots ----
     currentPlot(
-      ggplot(data = data,
-             mapping = aes(x = X, y = Y)) +
+      ggplot(
+        data = data,
+        mapping = aes(x = X, y = Y)
+      ) +
         geom_point(color = psuPalette[4], size = 4) +
         theme_bw() +
         labs(title = "Current Scatterplot") +
@@ -624,16 +643,17 @@ server <- function(input, output, clientData, session) {
 
   ## Add linear regression line ----
   observeEvent(input$options, {
-    if(input$options){
+    if (input$options) {
       output$plot1 <- renderPlot({
         currentPlot() +
           geom_smooth(
             method = "lm",
-            formula = y~x,
+            formula = y ~ x,
             se = FALSE,
             na.rm = TRUE,
             color = "blue",
-            size = 2)
+            size = 2
+          )
       })
     } else {
       output$plot1 <- renderPlot({
@@ -645,31 +665,46 @@ server <- function(input, output, clientData, session) {
   ## Performance Plot ----
   ## Track your performance and change the color of points for different level
   output$plot2 <- renderPlot({
-    if(is.null(tracking$DT) || nrow(tracking$DT) == 0){
+    if (is.null(tracking$DT) || nrow(tracking$DT) == 0) {
       basePerformPlot
     } else {
       basePerformPlot +
-        geom_point(data = tracking$DT,
-                   mapping = aes(
-                     x = actualValue,
-                     y = userGuess,
-                     color = difficulty,
-                     shape = difficulty),
-                   size = 4) +
-        scale_color_manual(name = "Difficulty",
-                           values = c("1" = boastUtils::psuPalette[2],
-                                      "2" = boastUtils::psuPalette[1],
-                                      "3" = boastUtils::psuPalette[6]),
-                           labels = c("Without Outlier",
-                                      "With Outlier",
-                                      "Surprise")) +
-        scale_shape_manual(name = "Difficulty",
-                           values = c("1" = 16,
-                                      "2" = 17,
-                                      "3" = 15),
-                           labels = c("Without Outlier",
-                                      "With Outlier",
-                                      "Surprise")) +
+        geom_point(
+          data = tracking$DT,
+          mapping = aes(
+            x = actualValue,
+            y = userGuess,
+            color = difficulty,
+            shape = difficulty
+          ),
+          size = 4
+        ) +
+        scale_color_manual(
+          name = "Difficulty",
+          values = c(
+            "1" = boastUtils::psuPalette[2],
+            "2" = boastUtils::psuPalette[1],
+            "3" = boastUtils::psuPalette[6]
+          ),
+          labels = c(
+            "Without Outlier",
+            "With Outlier",
+            "Surprise"
+          )
+        ) +
+        scale_shape_manual(
+          name = "Difficulty",
+          values = c(
+            "1" = 16,
+            "2" = 17,
+            "3" = 15
+          ),
+          labels = c(
+            "Without Outlier",
+            "With Outlier",
+            "Surprise"
+          )
+        ) +
         theme(
           legend.position = "bottom"
         )
@@ -685,31 +720,43 @@ server <- function(input, output, clientData, session) {
   ### Remaining hearts ----
   output$heart <- renderUI({
     if (hearts() == 5) {
-      img(src = "5hearts.png",
-          alt = "You have five hearts remaining",
-          width = '100%')
+      img(
+        src = "5hearts.png",
+        alt = "You have five hearts remaining",
+        width = "100%"
+      )
     } else if (hearts() == 4) {
-      img(src = "4hearts.png",
-          alt = "You have four hearts remaining",
-          width = '100%')
+      img(
+        src = "4hearts.png",
+        alt = "You have four hearts remaining",
+        width = "100%"
+      )
     } else if (hearts() == 3) {
-      img(src = "3hearts.png",
-          alt = "You have three hearts remaining",
-          width = '100%')
+      img(
+        src = "3hearts.png",
+        alt = "You have three hearts remaining",
+        width = "100%"
+      )
     } else if (hearts() == 2) {
-      img(src = "2hearts.png",
-          alt = "You have two hearts remaining",
-          width = '100%')
+      img(
+        src = "2hearts.png",
+        alt = "You have two hearts remaining",
+        width = "100%"
+      )
     } else if (hearts() == 1) {
-      img(src = "1heart.png",
-          alt = "You have one heart remaining",
-          width = '100%')
+      img(
+        src = "1heart.png",
+        alt = "You have one heart remaining",
+        width = "100%"
+      )
     } else {
       updateButton(session, "submit", disabled = TRUE)
       updateButton(session, "newplot", disabled = TRUE)
-      img(src = "gameisover.gif",
-          alt = "You have no hearts left; game over",
-          width = '100%')
+      img(
+        src = "gameisover.gif",
+        alt = "You have no hearts left; game over",
+        width = "100%"
+      )
     }
   })
 }
